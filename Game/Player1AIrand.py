f3f837dc0b9168372b6d0fb4b0d7df32bda0a9f5
GabrielKishida <gabriel.kishida@usp.br>
2023-07-16 02:44:28 +0900

    ⚡️ Add BFS

--- Game/Player1AIrand.py
+++ Game/Player1AIrand.py
@@ -1,4 +1,10 @@
-import random
+from collections import deque
+
+class BfsNode:
+        def __init__(self, position, source_direction, previous_node):
+            self.position = position
+            self.source_direction = source_direction
+            self.previous_node = previous_node
 
 class Player1AI:
 
@@ -21,78 +27,45 @@ class Player1AI:
             moves.append(('R',))
         return moves
 
-    def extract_path(self, start_position, target_x, board):
-        board_size = len(board)
-        current = start_position
-        path = []
-        visited = []
-
-        while current[0] != target_x:
-            #print("current",current)
-            #print("legal_directions",self.get_legal_directions(current))
-            found_valid_move = False
-
-            directions = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-            #print(directions )
+    # Does Breadth First Search and returns final destination node
+    def bfs(self, start_position, target_x, board):
+        start_node = BfsNode(start_position, None, None)
+        explored = []
+        
+        explored.append(start_position)
+        to_be_explored = deque([])
+        to_be_explored.append(start_node)
+        while to_be_explored:
+            current_node = to_be_explored.pop()
+            if current_node.position[0] == target_x:
+                return current_node
+            directions = self.get_legal_directions(current_node.position, board)
             for direction in directions:
-                neighbor = (current[0] + direction[0], current[1] + direction[1])
-                #print("neighbor", neighbor)
-                if neighbor in visited:
-                    path = []
-                    continue
-
-                
-                if neighbor == start_position or (neighbor[0] >= 0 and neighbor[0] < board_size and neighbor[1] >= 0 and neighbor[1] < board_size):
-                    #print("in bounds")
-                    if direction == (0, -1):
-                        forward_move = ('L',)
-                    elif direction == (0, 1):
-                        forward_move = ('R',)
-                    elif direction == (-1, 0):
-                        forward_move = ('U',)
-                    elif direction == (1, 0):
-                        forward_move = ('D',)
-
-                    #print("self.get_legal_directions(neighbor)", self.get_legal_directions(neighbor))
-                    if forward_move in self.get_legal_directions(current, board):
-                        path.append(forward_move)
-                        #print("appended", forward_move)
-                        #print("path", path)
-                        #print()
-                        visited.append(neighbor)
-                        current = neighbor
-                        found_valid_move = True
-                        break
-            #print("found_valid_move", found_valid_move)
-            if not found_valid_move:
-                # No valid move found, backtrack and continue exploring other paths
-                if not path:
-                    # No valid path exists, return False
-                    return False
-                else:
-                    # Remove the last move from the path and backtrack
-                    last_move = path.pop()
-##                    visited.pop()
-                    direction = None
-                    if last_move == ('L',):
-                        direction = (0, -1)
-                    elif last_move == ('R',):
-                        direction = (0, 1)
-                    elif last_move == ('D',):
-                        direction = (1, 0)
-                    elif last_move == ('U',):
-                        direction = (-1, 0)
-                    
-                    if direction:
-                        current = (current[0] - direction[0], current[1] - direction[1])
-            #print("current, start_position", current, start_position)
-
-        #print("out of while")    
+                neighbor = (0,0)
+                if direction == ('L',):
+                    neighbor = (current_node.position[0], current_node.position[1] - 1)
+                elif direction == ('R',):
+                    neighbor = (current_node.position[0], current_node.position[1] + 1)
+                elif direction == ('U',):
+                    neighbor = (current_node.position[0] - 1, current_node.position[1])
+                elif direction == ('D',):
+                    neighbor = (current_node.position[0] + 1, current_node.position[1])
+                if neighbor not in explored:
+                    explored.append(neighbor)
+                    to_be_explored.appendleft(BfsNode(neighbor,direction,current_node))
+        return False
+    
+    def bfs_get_path(self, start_position, target_x, board):
+        path = deque([])
+        current_node = self.bfs(start_position, target_x, board)
+        while current_node.source_direction:
+            path.appendleft(current_node.source_direction)
+            current_node = current_node.previous_node
         return path
 
 
     def get_move(self, game):
         p1_position = game.player_positions[game.players[0]]
-        path = self.extract_path(p1_position, 0,game.board)
+        path = self.bfs_get_path(p1_position, 0,game.board)
         print(path)
         return path[0]
