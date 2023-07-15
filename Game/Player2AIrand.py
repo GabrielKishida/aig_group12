0182d25aade713dd166cefa13b9a41635de355b3
GabrielKishida <gabriel.kishida@usp.br>
2023-07-16 01:24:47 +0900

    💡 Comment prints in player2

--- Game/Player2AIrand.py
+++ Game/Player2AIrand.py
@@ -3,5 +3,5 @@ import random
 class Player2AI:
     def get_move(self, game):
         legal_moves = game.get_legal_moves()
-        print("P2",legal_moves)
+        #print("P2",legal_moves)
         return random.choice(legal_moves)
